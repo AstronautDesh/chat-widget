371e8131acb334448ae90e1f85a85a11315baf04
AstronautDesh <162011931+AstronautDesh@users.noreply.github.com>
2024-10-21 06:01:43 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ client/README.md
@@ -0,0 +1,4 @@
+#Desh
+Chat Widget set up in components/ChatWidget.js
+responsive styling set for xs, md and lg viewports in css/chatwigdet.css
+
---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ChatWidget from './components/ChatWidget';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     Dev Dynasty....
+      </header>
+      <ChatWidget />
+    </div>
+  );
+}
+
+export default App;
